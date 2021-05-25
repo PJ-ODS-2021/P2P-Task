504043f0a5000c92d480480f356f716c2633390d
Jacob Rödel <roedel@campus.tu-berlin.de>
2021-05-25 13:14:17 +0200

    [OPTM-31] Add smoke tests for WebSocket connection

---
+++ test/websocket_server_custom.dart
@@ -0,0 +1,27 @@
+import 'dart:isolate';
+
+import 'package:p2p_task/network/socket_handler.dart';
+import 'package:p2p_task/utils/messages/debug_message.dart';
+
+void _registerTypes(SocketHandler sock) {
+  sock.registerTypename<DebugMessage>(
+      "DebugMessage", (json) => DebugMessage.fromJson(json));
+}
+
+void _registerServerCallbacks(SocketHandler sock) {
+  sock.registerCallback<DebugMessage>(
+      (msg) => print('server received debug message "${msg.value}"'));
+}
+
+startServer(SendPort sendPort) async {
+  var server = await runServer(0, (sock) async {
+    print('server got connection from client!');
+    _registerTypes(sock);
+    _registerServerCallbacks(sock);
+    sock.send(DebugMessage('hello from the server'));
+    await sock.listen();
+    await sock.close();
+  });
+
+  sendPort.send(server.port);
+}
