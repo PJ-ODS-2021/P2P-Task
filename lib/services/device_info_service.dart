7e8fc5d8e2a84f242c2bef895ee432960b959348
Jacob Rödel <roedel@campus.tu-berlin.de>
2021-06-04 14:41:29 +0200

    Add DeviceInfoService
    Provides human-readable device names which could be used later for the auto-discovery functionality.

---
+++ lib/services/device_info_service.dart
@@ -0,0 +1,28 @@
+import 'dart:io';
+
+import 'package:device_info_plus/device_info_plus.dart';
+import 'package:flutter/foundation.dart';
+
+class DeviceInfoService {
+  final DeviceInfoPlugin _deviceInfoPlugin;
+
+  DeviceInfoService(DeviceInfoPlugin? deviceInfoPlugin)
+      : this._deviceInfoPlugin = deviceInfoPlugin ?? DeviceInfoPlugin();
+
+  Future<String> get deviceName async {
+    if (kIsWeb)
+      return await (await _deviceInfoPlugin.webBrowserInfo).userAgent;
+    else if (Platform.isIOS)
+      return (await _deviceInfoPlugin.iosInfo).model ?? 'iOS';
+    else if (Platform.isAndroid)
+      return (await _deviceInfoPlugin.androidInfo).model ?? 'Android';
+    else if (Platform.isLinux)
+      return (await _deviceInfoPlugin.linuxInfo).prettyName;
+    else if (Platform.isMacOS)
+      return (await _deviceInfoPlugin.macOsInfo).model;
+    else if (Platform.isWindows)
+      return (await _deviceInfoPlugin.windowsInfo).computerName;
+    else
+      return 'Unknown';
+  }
+}
