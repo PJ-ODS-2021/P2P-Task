aa2f7443b853aa16313b09eb5a399ccdf137b526
Kons <konss97@gmail.com>
2021-06-05 16:20:57 +0200

    OPTM-49 Minor refactoring in LogMixin internals. Added colored logging for warning and severe.

--- lib/utils/log_mixin.dart
+++ lib/utils/log_mixin.dart
@@ -3,32 +3,36 @@ import 'dart:io';
 import 'package:flutter/foundation.dart';
 import 'package:logging/logging.dart';
 
-mixin LogMixin on Object {
-  static const String GREY = '\x1B[37m';
+mixin LogMixin {
+  static const String WHITE = '\x1B[37m';
+  static const String YELLOW = '\x1B[33m';
+  static const String RED = '\x1B[31m';
   static const String RESET = '\x1B[0m';
-  static Map<String, Logger> _logger = {};
+  static Map<String, Logger> _loggers = {};
 
-  String get _className => this.runtimeType.toString();
-  late final Logger l = _logger[_className] ?? createLogger();
+  String get _className => runtimeType.toString();
 
-  createLogger() {
-    _logger[_className] = Logger(_className)..clearListeners();
-    if (kReleaseMode ||
-        (!kIsWeb && Platform.environment.containsKey('FLUTTER_TEST')))
-      return _logger[_className];
+  late final Logger l = _loggers[_className] ?? createLogger();
 
-    return _logger[_className]!
-      ..onRecord.listen(
-        (record) {
-          debugPrint(
-              '$GREY[${record.level.name}]$RESET ${record.time} $GREY${record.loggerName}$RESET: ${record.message}' +
-                  (record.error != null
-                      ? '\n$GREY>$RESET ${record.error}'
-                      : '') +
-                  (record.stackTrace != null
-                      ? '\n$GREY>$RESET ${record.stackTrace}'
-                      : ''));
-        },
-      );
+  static bool get _useColoredDebugPrint =>
+      !kReleaseMode &&
+      (kIsWeb || !Platform.environment.containsKey('FLUTTER_TEST'));
+
+  Logger createLogger() {
+    final logger = _loggers[_className] = Logger(_className)..clearListeners();
+    if (_useColoredDebugPrint) logger.onRecord.listen(_coloredPrint);
+    return logger;
+  }
+
+  void _coloredPrint(LogRecord record) {
+    final levelColor = record.level <= Level.INFO
+        ? WHITE
+        : (record.level <= Level.WARNING ? YELLOW : RED);
+    debugPrint(
+        '$levelColor[${record.level.name}]$RESET ${record.time} $WHITE${record.loggerName}$RESET: ${record.message}' +
+            (record.error != null ? '\n$WHITE>$RESET ${record.error}' : '') +
+            (record.stackTrace != null
+                ? '\n$WHITE>$RESET ${record.stackTrace}'
+                : ''));
   }
 }
