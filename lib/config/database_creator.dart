9a7cce2ec6edd0826f4688b1f459cff00c4a4c7d
Kons <konss97@gmail.com>
2021-06-10 14:24:36 +0200

    OPTM-49 Removed requesting of Permission.storage (should work without it)

--- lib/config/database_creator.dart
+++ lib/config/database_creator.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/foundation.dart';
 import 'package:path/path.dart';
 import 'package:path_provider/path_provider.dart';
-import 'package:permission_handler/permission_handler.dart' as perm;
 import 'package:sembast/sembast.dart';
 import 'package:sembast/sembast_io.dart';
 import 'package:sembast_web/sembast_web.dart';
@@ -13,19 +10,12 @@ class DatabaseCreator {
 
   static Future<Database> create() async {
     if (_db != null) return _db!;
-    if (kIsWeb) {
-      _db = await databaseFactoryWeb.openDatabase('p2p_task');
-    } else {
-      // Need to check for Android or iOS permissions
-      if (Platform.isAndroid || Platform.isIOS) {
-        final status = await perm.Permission.storage.request();
-        if (!status.isGranted) exit(-1);
-      }
-      final dir = await getApplicationDocumentsDirectory();
-      await dir.create(recursive: true);
-      var dbPath = join(dir.path, 'p2p_task.db');
-      _db = await databaseFactoryIo.openDatabase(dbPath);
-    }
-    return _db!;
+    if (kIsWeb) return await databaseFactoryWeb.openDatabase('p2p_task');
+
+    // no need to request Permission.storage
+    final dir = await getApplicationDocumentsDirectory();
+    await dir.create(recursive: true);
+    var dbPath = join(dir.path, 'p2p_task.db');
+    return _db = await databaseFactoryIo.openDatabase(dbPath);
   }
 }
