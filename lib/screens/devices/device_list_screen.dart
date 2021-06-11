896a52abee098d1fc91063ad7607630fbe3a2f6c
Kons <konss97@gmail.com>
2021-06-11 15:22:24 +0200

    OPTM-49 Removed unused import

--- lib/screens/devices/device_list_screen.dart
+++ lib/screens/devices/device_list_screen.dart
@@ -1,4 +1,3 @@
-import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:p2p_task/config/style_constants.dart';
