1e00b224345f225ea220bcc16ca8fb7bdebefc49
Kons <konss97@gmail.com>
2021-06-16 17:20:16 +0200

    OPTM-76 Show dialog asking to start server if it is not running when opening QR screen.
    OPTM-76 #close

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -5,7 +5,10 @@ import 'package:p2p_task/screens/devices/device_list_screen.dart';
 import 'package:p2p_task/screens/qr_code_dialog.dart';
 import 'package:p2p_task/screens/settings/settings_screen.dart';
 import 'package:p2p_task/screens/task_list_screen.dart';
+import 'package:p2p_task/services/change_callback_notifier.dart';
+import 'package:p2p_task/services/peer_service.dart';
 import 'package:p2p_task/widgets/bottom_navigation.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatefulWidget {
   final String title;
@@ -27,8 +30,7 @@ class _HomeScreenState extends State<HomeScreen> {
         centerTitle: true,
         actions: [
           IconButton(
-            onPressed: () => showDialog(
-                context: context, builder: (context) => QrCodeDialog()),
+            onPressed: () => _openQrCodeDialog(context),
             icon: Icon(Icons.qr_code),
           ),
         ],
@@ -54,5 +56,47 @@ class _HomeScreenState extends State<HomeScreen> {
         }),
       ),
     );
+  }
+
+  void _openQrCodeDialog(BuildContext context) {
+    final peerService =
+        Provider.of<ChangeCallbackNotifier<PeerService>>(context, listen: false)
+            .callbackProvider;
+    if (!peerService.isServerRunning) {
+      showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+                title: const Text('The server is not running!'),
+                content: SingleChildScrollView(
+                  child: ListBody(
+                    children: const [
+                      Text('Clients won\'t be able to connect to this device.'),
+                      Text('Would you like to start the server now?'),
+                    ],
+                  ),
+                ),
+                actions: [
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                        peerService.startServer();
+                        showDialog(
+                            context: context,
+                            builder: (context) => QrCodeDialog());
+                      },
+                      child: Text('Yes')),
+                  TextButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                        showDialog(
+                            context: context,
+                            builder: (context) => QrCodeDialog());
+                      },
+                      child: Text('No')),
+                ],
+              ));
+    } else {
+      showDialog(context: context, builder: (context) => QrCodeDialog());
+    }
   }
 }
