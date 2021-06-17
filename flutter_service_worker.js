494cbecef73091dbf9c38b6a161fd5b27366f810
JacobRoe <jacob.friedhart@gmail.com>
2021-06-17 08:17:20 +0000

    Deploying to gh-pages from @ PJ-ODS-2021/P2P-Task@2921250de5a9268c1bbf6c2cc2aa463edde4a69f 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,19 +3,19 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "version.json": "71f03b53893ddeae34aad311c5972fa9",
-"main.dart.js": "326aeb4a393c59b9b317ffff8f2e1c76",
-"index.html": "b77be66517ac72484dbf2e574df781de",
-"/": "b77be66517ac72484dbf2e574df781de",
-"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
-"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
-"assets/NOTICES": "f1d188495c0b2650290292f5422925d3",
-"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
+  "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"version.json": "71f03b53893ddeae34aad311c5972fa9",
 "manifest.json": "45a969375463b630f6867976e3bb2264",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796"
+"assets/AssetManifest.json": "2efbb41d7877d10aac9d091f58ccd7b9",
+"assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
+"assets/NOTICES": "93d2d15288ad9be4ac3a122efcd6ac2b",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
+"favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"main.dart.js": "53c4a210f8dccf8d52aef7adeba6af95",
+"index.html": "b17452ac52676dc33e95611396419ad4",
+"/": "b17452ac52676dc33e95611396419ad4"
 };
 
 // The application shell files that are downloaded before a service worker can
