1d6679cbeb141176f18af52efb8824157dfc9291
PitchBlackCoffee <1965426+PitchBlackCoffee@users.noreply.github.com>
2021-06-30 11:37:55 +0000

    Deploying to gh-pages from @ PJ-ODS-2021/P2P-Task@d2f63e93eb1b5d74a57dff59fca2c13544e21c70 🚀

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,21 +3,21 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
-  "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
-"version.json": "71f03b53893ddeae34aad311c5972fa9",
+  "favicon.png": "5dcef449791fa27946b3d35ad8803796",
+"main.dart.js": "9955882b0a4c0241ced132a7157e1479",
+"index.html": "72e6882be44bc4dddb99596966c88021",
+"/": "72e6882be44bc4dddb99596966c88021",
 "manifest.json": "45a969375463b630f6867976e3bb2264",
-"assets/AssetManifest.json": "36dd4c2f1b0fdb1f7b7e723c8beaf428",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
+"assets/NOTICES": "52c9145c93726913d59fb8639b17c66a",
+"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "4e6447691c9509f7acdbf8a931a85ca1",
 "assets/assets/up_arrow_icon.png": "ad0578182f5e47659fa4d88220a0a609",
 "assets/assets/down_arrow_icon.png": "9d7afd84ca81113e51b56cd6ed6104c6",
-"assets/NOTICES": "d98d3022fadaa64ea9c69f60523a889b",
-"assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
-"favicon.png": "5dcef449791fa27946b3d35ad8803796",
-"main.dart.js": "a5295f1073a3b9f1785e3fdc192645e9",
-"index.html": "db9449ff5bf1d46a8192b6bd84f51d93",
-"/": "db9449ff5bf1d46a8192b6bd84f51d93"
+"assets/AssetManifest.json": "36dd4c2f1b0fdb1f7b7e723c8beaf428",
+"version.json": "71f03b53893ddeae34aad311c5972fa9",
+"icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
+"icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1"
 };
 
 // The application shell files that are downloaded before a service worker can
